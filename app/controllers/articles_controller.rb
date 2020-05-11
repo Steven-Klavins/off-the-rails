309edf9e63a10bcc1f150fd06e8b7e1b26efb316
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-11 04:08:42 +0100

    added rspec in rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
     def new
     end
+    
+    def create
+    render plain: params[:article].inspect
+    @article.save
+    redirect_to @article
+  end
 end
+
+
