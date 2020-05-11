6da84cd520cf72ba60a64b46b39a5f5ff2f65647
Steven <Steven@Stevens-iMac.local>
2020-05-11 06:18:36 +0100

    first capybara/rspec test up and running, app now shows post on submit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,23 @@ class ArticlesController < ApplicationController
     def new
     end
     
-    def create
-    render plain: params[:article].inspect
+def create
+    @article = Article.new(article_params)
     @article.save
     redirect_to @article
-  end
 end
+
+def show
+    @article = Article.find(params[:id])
+end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
+
+
 
 
