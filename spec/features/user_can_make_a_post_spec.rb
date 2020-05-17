8951c887e3938e343ad0b67a6a658ac4edab56ae
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-17 22:25:03 +0100

    added new feature test, now passing

--- spec/features/user_can_make_a_post_spec.rb
+++ spec/features/user_can_make_a_post_spec.rb
@@ -4,9 +4,19 @@ RSpec.feature "Make Post", type: :feature do
   scenario "Can submit posts" do
     visit "articles/new"
     fill_in "article_title", with: "My new post"
-    fill_in "article_text", with: "Hello here are is some random text"
+    fill_in "article_text", with: "Hello here is some random text"
     click_button "Save Article"
     expect(page).to have_content("My new post")
-    expect(page).to have_content("Hello here are is some random text")
+    expect(page).to have_content("Hello here is some random text")
   end
+
+    scenario "A user adds an article to the main list" do
+      visit "articles/new"
+      fill_in "article_title", with: "This awesome new post"
+      fill_in "article_text", with: "Hello here is some random text"
+      click_button "Save Article"
+      visit "/articles"
+      expect(page).to have_content("This awesome new post")
+      expect(page).to have_content("Hello here is some random text")
+    end
 end
