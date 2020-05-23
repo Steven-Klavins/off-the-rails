fc1bdcbb8aaab51c5fb726d76469377a3900e8d3
Steven <Steven@Stevens-iMac.local>
2020-05-23 22:21:00 +0100

    Added another RSpec test, now passing

---
+++ spec/models/articles_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Make Post", type: :feature do
+  scenario "User will not able to submit a post with a title less than 5 characters" do
+    visit "articles/new"
+    fill_in "article_title", with: "abc"
+    fill_in "article_text", with: "Hello here is some random text"
+    click_button "Save Article"
+    expect(page).to have_content("Create a new off the rails article")
+    visit "/articles"
+    expect(page).should have_no_content("abc") 
+  end
+end
