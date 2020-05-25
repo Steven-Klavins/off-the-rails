344ab4e5869e32425eda6774808f271baefec87b
Steven <Steven@Stevens-iMac.local>
2020-05-25 20:54:33 +0100

    User can now delete posts, working but no test due to issues with capybara webkit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,14 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-        end
+  end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
