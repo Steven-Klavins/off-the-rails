79a816c2bf79ac501fa6cc23fc108027accdd7f9
Steven <Steven@Stevens-iMac.local>
2020-05-31 21:43:59 +0100

    Test: user can make a comment

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -12,4 +12,18 @@ RSpec.feature "A user can comment on a post", type: :feature do
         expect(page).to have_content("Commenter: Jack")
         expect(page).to have_content("Comment: Here is an awesome comment")
       end
+
+      scenario "User can make comments and delete them" do
+        visit "articles/new"
+        fill_in "article_title", with: "My new post"
+        fill_in "article_text", with: "Hello here is some random text"
+        click_button "Create Article"
+        fill_in "Commenter", with: "Jack"
+        fill_in "Body", with: "Here is an awesome comment"
+        click_button "Create Comment"
+        expect(page).to have_content("Commenter: Jack")
+        expect(page).to have_content("Comment: Here is an awesome comment")
+        click_on('Delete')
+      end
+
 end
