ff7ca1e916b0f409ef0e3eb31b7d30443dd83301
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-01 05:13:12 +0100

    Asset: changed app asset config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,10 +9,11 @@ module OffTheRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('/app/assets/fonts') 
   end
 end
